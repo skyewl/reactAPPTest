420be2924e777ea3e7974c4e1ed5f377141f19f3
skye_git <skye_wy@163.com>
2020-03-06 11:22:26 +0800

    提交

---
+++ build/precache-manifest.2231cec7dcf52d416e7956ab37f7bbfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615e638c7b0d3a6118d0cb4da1097dc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7285034eda57b9eb3ff4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b27f6fb8cef3af8da60e",
+    "url": "/static/js/2.89e20e19.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.89e20e19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7285034eda57b9eb3ff4",
+    "url": "/static/js/main.ffa5fcd9.chunk.js"
+  },
+  {
+    "revision": "95b958343a19ee72e1dc",
+    "url": "/static/js/runtime-main.d3215492.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
